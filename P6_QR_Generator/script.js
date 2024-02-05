68604b8017c757b61aa5cd3a51513fdbdc3d7375
sumitkpal62 <sciencebuddy51@gmail.com>
2024-02-05 19:17:16 +0530

    Project 6

---
+++ P6_QR_Generator/script.js
@@ -0,0 +1,22 @@
+const BASE_URL = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=';
+
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+let btn = document.getElementById("generateQR");
+
+
+const generateQR = () => {
+    if (qrText.value.length > 0) {
+        qrImage.src = BASE_URL + qrText.value;
+        imgBox.classList.add("show-img");
+    }
+    else {
+        qrText.classList.add("error");
+        setTimeout(() => {
+            qrText.classList.remove("error");
+        }, 1000);
+    }
+}
+
+btn.addEventListener("click", generateQR);
