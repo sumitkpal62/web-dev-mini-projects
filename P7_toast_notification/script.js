bd5c821a5df8c0ce53ae0617730086dc58889fe8
sumitkpal62 <sciencebuddy51@gmail.com>
2024-02-06 14:25:17 +0530

    animation added to toast notification

--- P7_toast_notification/script.js
+++ P7_toast_notification/script.js
@@ -3,6 +3,8 @@ let successBtn = document.getElementById("success");
 let errorBtn = document.getElementById("error");
 let invalidBtn = document.getElementById("invalid");
 let successMsg = '<i class="fa-solid fa-circle-check"></i> Submit Successfully.';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Something wrong, fix the error!';
+let invalidMsg = '<i class="fa-solid fa-triangle-exclamation"></i> Invalid entry. Try Again.';
 
 const selectNotification = (target) => {
     if (target.value == 'Success') {
@@ -19,7 +21,7 @@ const popNotification = () => {
     setTimeout(() => {
         toastBox.removeChild(newNotification);
 
-    }, 2000);
+    }, 5000);
 }
 
 // button.addEventListener("click", popNotification);
@@ -33,31 +35,31 @@ successBtn.addEventListener("click", (evt) => {
     setTimeout(() => {
         toastBox.removeChild(newNotification);
 
-    }, 2000);
+    }, 5000);
 })
 
 errorBtn.addEventListener("click", (evt) => {
     let newNotification = document.createElement("div");
     newNotification.classList.add("notification");
-    newNotification.innerHTML = '<i class="fa-solid fa-circle-xmark"></i> Something wrong, fix the error!';
+    newNotification.innerHTML = errorMsg;
     newNotification.style.color = 'red'
     toastBox.appendChild(newNotification);
     toastBox.scrollTop = toastBox.scrollHeight - toastBox.clientHeight;
     setTimeout(() => {
         toastBox.removeChild(newNotification);
 
-    }, 2000);
+    }, 5000);
 })
 
 invalidBtn.addEventListener("click", (evt) => {
     let newNotification = document.createElement("div");
     newNotification.classList.add("notification");
-    newNotification.innerHTML = '<i class="fa-solid fa-triangle-exclamation"></i> Invalid entry. Try Again.';
-    newNotification.style.color = '#a7a415'
+    newNotification.innerHTML = invalidMsg;
+    newNotification.style.color = 'orange'
     toastBox.appendChild(newNotification);
     toastBox.scrollTop = toastBox.scrollHeight - toastBox.clientHeight;
     setTimeout(() => {
         toastBox.removeChild(newNotification);
 
-    }, 2000);
+    }, 5000);
 })
