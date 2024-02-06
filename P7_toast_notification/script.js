9dabf395f495401fb6d93ee24f9cb4871df66a47
sumitkpal62 <sciencebuddy51@gmail.com>
2024-02-06 17:05:49 +0530

    animation bug fixed

--- P7_toast_notification/script.js
+++ P7_toast_notification/script.js
@@ -2,20 +2,16 @@ let toastBox = document.querySelector(".toast-notification");
 let successBtn = document.getElementById("success");
 let errorBtn = document.getElementById("error");
 let invalidBtn = document.getElementById("invalid");
-let successMsg = '<i class="fa-solid fa-circle-check"></i> Submit Successfully.';
-let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Something wrong, fix the error!';
-let invalidMsg = '<i class="fa-solid fa-triangle-exclamation"></i> Invalid entry. Try Again.';
+let successMsg = '<i class="fa-solid fa-circle-check success"></i> Submit Successfully.';
+let errorMsg = '<i class="fa-solid fa-circle-xmark error"></i> Something wrong, fix the error!';
+let invalidMsg = '<i class="fa-solid fa-triangle-exclamation invalid"></i> Invalid entry. Try Again.';
 
-const selectNotification = (target) => {
-    if (target.value == 'Success') {
 
-    }
-}
-
-const popNotification = () => {
+const popNotification = (msg, msgClass) => {
     let newNotification = document.createElement("div");
     newNotification.classList.add("notification");
-    newNotification.innerText = "Success";
+    newNotification.classList.add(msgClass);
+    newNotification.innerHTML = msg;
     toastBox.appendChild(newNotification);
     toastBox.scrollTop = toastBox.scrollHeight - toastBox.clientHeight;
     setTimeout(() => {
@@ -24,42 +20,15 @@ const popNotification = () => {
     }, 5000);
 }
 
-// button.addEventListener("click", popNotification);
-successBtn.addEventListener("click", (evt) => {
-    let newNotification = document.createElement("div");
-    newNotification.classList.add("notification");
-    newNotification.innerHTML = successMsg;
-    newNotification.style.color = 'green'
-    toastBox.appendChild(newNotification);
-    toastBox.scrollTop = toastBox.scrollHeight - toastBox.clientHeight;
-    setTimeout(() => {
-        toastBox.removeChild(newNotification);
-
-    }, 5000);
-})
-
-errorBtn.addEventListener("click", (evt) => {
-    let newNotification = document.createElement("div");
-    newNotification.classList.add("notification");
-    newNotification.innerHTML = errorMsg;
-    newNotification.style.color = 'red'
-    toastBox.appendChild(newNotification);
-    toastBox.scrollTop = toastBox.scrollHeight - toastBox.clientHeight;
-    setTimeout(() => {
-        toastBox.removeChild(newNotification);
-
-    }, 5000);
+successBtn.addEventListener("click", () => {
+    popNotification(successMsg, "success");
 })
 
 invalidBtn.addEventListener("click", (evt) => {
-    let newNotification = document.createElement("div");
-    newNotification.classList.add("notification");
-    newNotification.innerHTML = invalidMsg;
-    newNotification.style.color = 'orange'
-    toastBox.appendChild(newNotification);
-    toastBox.scrollTop = toastBox.scrollHeight - toastBox.clientHeight;
-    setTimeout(() => {
-        toastBox.removeChild(newNotification);
-
-    }, 5000);
+    popNotification(invalidMsg, "invalid");
 })
+
+errorBtn.addEventListener("click", () => {
+    popNotification(errorMsg, "error");
+})
+
